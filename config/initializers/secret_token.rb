3c7f2a57c33179d67cb997087f49a5ea18fe8dfa
Rajesh Rathod <rajesh.rathod@corporategovernancerisk.com>
2012-11-15 03:20:40 +0530

    bbone commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = 'f1a764c7e4a8bbac13ac14c470583712ec01ce96b758acda598fec431d5f0f3ebda07e15012757c27152309b6f5473bc10799b4c971fd7610e4777b8a440fe3f'
---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Entry.create!(name: "Rajesh")
+Entry.create!(name: "Aarthi Raj")
+Entry.create!(name: "DHH")
+Entry.create!(name: "Callum jones")
+Entry.create!(name: "Steve jobs")
+Entry.create!(name: "Banoth Valiya")
+Entry.create!(name: "Aaron Patterson")
